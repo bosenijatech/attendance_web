0cb271e10487aad71cec42eaba2149b6e5a88322
bosenijatech <bose.g@nijatech.com>
2025-12-11 13:51:13 +0530

    url update

--- lib/constant/app_constants.dart
+++ lib/constant/app_constants.dart
@@ -2,7 +2,8 @@ class AppConstants {
   // static const appVersion = '1.0.4';
  
 
-   static const apiBaseUrl ="https://attendance-nodejs-iqgz.onrender.com/"; // live
+  // static const apiBaseUrl ="https://attendance-nodejs-iqgz.onrender.com/"; // live
+   static const apiBaseUrl ="https://attendance-nodejs.onrender.com/"; // live
   //  static const apiBaseUrl ="http://192.168.0.73:3000/"; // live
   //  static const apiBaseUrl ="http://10.208.213.121:3000/"; // live
   //  static const apiBaseUrl ="http://10.94.144.121:3000/"; // live
